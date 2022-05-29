2fc1f1aba47230c739f9e68d6a4f5b996609c21e
cpe-thesis <106480776+cpe-thesis@users.noreply.github.com>
2022-05-30 03:28:13 +0800

    Update mapvis.py

--- pages/mapvis.py
+++ pages/mapvis.py
@@ -27,13 +27,13 @@ def app():
 
     if(vis == "Flow of Outbreak"):
         try:
-            components.iframe("https://kristineirishhh.github.io/map_visualisation/", height=1000)
+            components.iframe("https://kristineirishhh.github.io/map_visualisation/", height=1100)
 
         except:
              pass
     elif(vis == "Outbreak Severities"):
         try:
-            components.iframe("https://kristineirishhh.github.io/outbreak_severities/", height=1000)
+            components.iframe("https://kristineirishhh.github.io/outbreak_severities/", height=1100)
         except:
              pass
 
